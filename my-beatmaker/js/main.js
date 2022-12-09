412787d165e5f64155071d90bd5e2fc06c3db1e8
ike_icon <asieduisaac5775@gmail.com>
2022-12-09 22:30:53 +0000

    edit beat maker

--- my-beatmaker/js/main.js
+++ my-beatmaker/js/main.js
@@ -2,30 +2,31 @@ window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
   const visuals = document.querySelector(".visual");
-  const colors = [
-    "#6ed187",
-    "#FF5C33",
-    "gold",
-    "aqua",
-    "#21003D",
-    "hotpink"
-  ]
+  const colors = ["#6ed187", "#FF5C33", "gold", "aqua", "#21003D", "hotpink"];
 
   pads.forEach((pad, index) => {
     pad.addEventListener("click", () => {
       sounds[index].currentTime = 0;
       sounds[index].play();
-      jumpingBalls(index)
+      jumpingBalls(index);
     });
+
+    pad.addEventListener("keypress", (event) => {
+      let key = event.key;
+      if (key === 'w') {
+        console.log('event.key');}
+    });
+   
   });
 
-  function jumpingBalls(index){
+
+  function jumpingBalls(index) {
     const balls = document.createElement("div");
     visuals.appendChild(balls);
     balls.style.backgroundColor = colors[index];
     balls.style.animation = `jump 1s ease`;
-    balls.addEventListener("animationend", function() {
+    balls.addEventListener("animationend", function () {
       visuals.removeChild(this);
     });
-  };
+  }
 });
